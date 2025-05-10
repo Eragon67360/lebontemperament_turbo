492d8f987211e64b60fe243d244109b9cf158c01
Thomas Moser <thomas.moser@avenit.de>
2025-05-10 22:00:32 +0200

    ✨ added rehearsals

---
+++ apps/admin/app/dashboard/members/repetitions/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import RehearsalsList from "@/components/RehearsalsList";
+
+export default function Repetitions() {
+  return (
+    <div>
+      <RehearsalsList />
+    </div>
+  );
+}
