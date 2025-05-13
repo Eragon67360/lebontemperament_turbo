c022417e2f96e85660b5caa0d3270e325f9c185a
Thomas Moser <thomas.moser@avenit.de>
2025-05-13 23:17:46 +0200

    feat: added rehearsals to public part

---
+++ apps/website/app/api/rehearsals/route.ts
@@ -0,0 +1,24 @@
+import { createClient } from "@/utils/supabase/server";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    const supabase = await createClient();
+
+    const { data, error } = await supabase
+      .from("rehearsals")
+      .select("*")
+      .order("date", { ascending: true });
+
+    if (error) throw error;
+
+    return NextResponse.json(data);
+  } catch (error) {
+    console.error(error);
+
+    return NextResponse.json(
+      { error: "Error fetching rehearsals" },
+      { status: 500 }
+    );
+  }
+}
