fb9100033ee5fdd61aebdb7a2c5dccfdf620e517
Thomas Moser <thomas.moser@avenit.de>
2026-02-05 14:23:50 +0100

    feat: implemented support contact feature with email submission and UI updates

---
+++ apps/mobile_app/lib/core/constants/support_constants.dart
@@ -0,0 +1,7 @@
+/// Support contact subject options for the Aide & Contact form.
+const List<String> kSupportSubjectOptions = [
+  'Bug / Problème technique',
+  'Suggestion d\'amélioration',
+  'Question générale',
+  'Autre',
+];
